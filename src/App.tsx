563100536337f1f0852a203ba16ec9c6f4e4715d
RafaelM <rafael.mattos09@gmail.com>
2026-08-29 19:44:23 -0300

    23.ComponentLogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './styles/theme.css'
 import './styles/global.css'
-
-
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
       <Container>
         <Heading>Menu</Heading>
